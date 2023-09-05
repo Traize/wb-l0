1493bd95658f2a81da78e24bdbefb84ba0033aee
Traize <bishopdc@yandex.ru>
2023-09-05 14:00:33 +0300

    2

--- js/missingItemTemplate.js
+++ js/missingItemTemplate.js
@@ -4,13 +4,14 @@ export const missingEl = missingModel
         return `<div id="missingItem" class="item-template missing-item">
                 <div class="list-item missing-list">
                     <div class="list-item__obj">
+                         <div class="mobile-size ${!(item.mobileSize) ? "mobile-size-hidden" : ""}">${item.mobileSize}</div>
                         <div class="img-wrapper"><img src="${item.img}" class="item-img missing-img"></img></div>
                     </div>
                         <div id="listItemDescription" class="list-item__description">
                             <div id="itemTitle" class="h3 item-title" data-model-item${item.id}>${item.title}</div>
                             <div id="itemProps" class="h4 item__property ${!(item.props.color || item.props.size) ? "item__property-hidden" : ""}">
                                 <div id="propsColor">${item.props.color}</div>
-                                <div id="propsSize">${item.props.size}</div>
+                                <div id="propsSize" class="props-size">${item.props.size}</div>
                             </div>
                         </div>
                     </div>
